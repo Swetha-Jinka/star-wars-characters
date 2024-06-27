597bba1908b19f166979272117509a5fbdea0404
balu <balu.n408@gmail.com>
2024-06-27 21:31:52 +0530

    SWAPI character

---
+++ src/page1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomePage from './HomePage'
+ 
+
+
+export default function Page1() {
+    return (
+        <div>
+            <h1>Page 1</h1>
+         
+ 
+
+            <HomePage />
+        </div>
+    )
+}
